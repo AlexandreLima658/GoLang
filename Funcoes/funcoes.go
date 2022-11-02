06a8be08a1ca25b300a78c7798942c5f6e16174e
Alexandre <alexandrec@alu.ufc.br>
2022-11-02 17:24:11 -0300

    Atualização

--- Funcoes/funcoes.go
+++ Funcoes/funcoes.go
@@ -6,6 +6,12 @@ func somar(n1 int8, n2 int8) int8 {
 	return n1 + n2
 }
 
+func calculosMatematicos(n1, n2 int8) (int8, int8) {
+	soma := somar(n1, n2)
+	subtracao := n1 - n2
+	return soma, subtracao
+}
+
 func main() {
 	soma := somar(10, 15)
 	fmt.Println("A soma é", soma)
@@ -18,4 +24,7 @@ func main() {
 	resultado := f("Texto da função")
 	fmt.Println(resultado)
 
+	resultadoSoma, resultadoSubtracao := calculosMatematicos(10, 15)
+	fmt.Println(resultadoSoma, resultadoSubtracao)
+	
 }
